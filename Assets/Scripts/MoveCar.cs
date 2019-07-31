d3912120f5d3249738162aee43aec55c65c78dc2
sunydeprito <prito.suny@gmail.comom>
2019-07-31 11:21:36 +0700

    Add Assets and project settings
    Add Assets and project settings

---
+++ Assets/Scripts/MoveCar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCar : MonoBehaviour
+{
+
+    public float moveSpeed = 200;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(gameObject);
+        print("Crash");
+    }
+
+
+}
---
+++ Assets/Scripts/Setting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class Setting : MonoBehaviour {
+ 
+   // teks representasi untuk persen
+   Text persenTeks;
+ 
+   // Use this for initialization
+   void Start() {
+       persenTeks = GetComponent<Text>();
+ 
+
+   }
+ 
+   // toggle untuk fullscreen
+   public void TampilanPenuh (bool isFullscreen) {
+       Screen.fullScreen = isFullscreen;
+   }
+
+ 
+   // teks untuk persen
+   public void Volume (float value) {
+       persenTeks.text = Mathf.RoundToInt(value * 100) + "%";
+   }
+}
